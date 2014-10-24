a759d756bea8436e42ee9125bd81717a0ac5bc37
Nicola Justus <nicola.justus@mni.thm.de>
2014-10-24 13:50:04 +0200

    file structure redesigned

--- web.js
+++ web.js
@@ -6,8 +6,13 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
 app.use(express.logger());
-
 app.use(express.static(__dirname + '/public'));
+
+// use all needed folders
+app.use(express.static(__dirname + '/bootstrap-3.2.0-dist');
+app.use(express.static(__dirname + '/css');
+app.use(express.static(__dirname + '/img');
+app.use(express.static(__dirname + '/javascript');
 
 var domain = process.env.DOMAIN || '';
 var port = process.env.PORT || 5000;
